2fea4089efe43207d614933243f00f78779833aa
Alexandria Ellis <allstarcoding777@outlook.com>
2023-04-03 23:25:56 -0500

    webpack config plugins added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,30 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+webpackConfig.plugins.push(
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+  }),
+  new WebpackPwaManifest({
+    name: 'Budget Tracker',
+    short_name: 'Budget',
+    description: 'An application for tracking your budget',
+    start_url: '/',
+    icons: [
+      {
+        src: path.resolve('src/images/icons/icon-192x192.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join('assets', 'icons'),
+      },
+    ],
+  }),
 // TODO: Add CSS loaders and babel to webpack.
+  new InjectManifest({
+    swSrc: './src/service-worker.js',
+    swDest: 'service-worker.js',
+  })
+);
 
 module.exports = () => {
   return {
