4bb85a3601398a6f805550336218ac205307fdfe
Alexandria Ellis <allstarcoding777@outlook.com>
2023-04-03 23:28:38 -0500

    asset catching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
 registerRoute();
